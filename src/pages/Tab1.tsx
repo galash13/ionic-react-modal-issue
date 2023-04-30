6036debdf8b8154d0b2c483cef954c15ddb4bbf8
Gabriele Messina <zankle95@gmail.com>
2023-04-30 17:46:52 +0200

    added modal and ios proj

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,19 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonModal,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Tab1.css";
+import { useState } from "react";
 
 const Tab1: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +27,27 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonButton expand="block" onClick={() => setIsOpen(true)}>
+          Open
+        </IonButton>
+        <IonModal isOpen={isOpen}>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>Modal</IonTitle>
+              <IonButtons slot="end">
+                <IonButton onClick={() => setIsOpen(false)}>Close</IonButton>
+              </IonButtons>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className="ion-padding">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+              illum quidem recusandae ducimus quos reprehenderit. Veniam,
+              molestias quos, dolorum consequuntur nisi deserunt omnis id illo
+              sit cum qui. Eaque, dicta.
+            </p>
+          </IonContent>
+        </IonModal>
       </IonContent>
     </IonPage>
   );
